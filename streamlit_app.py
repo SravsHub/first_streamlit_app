3f0b6a681f016d52bb796abd37a9432d7d8e452c
SravsHub <107316109+SravsHub@users.noreply.github.com>
2023-02-03 14:58:32 -0500

    Update streamlit_app.py
    adding emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ streamlit.title("My parents new healthy dinner")
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build your own smoothie 🥝🍇')
