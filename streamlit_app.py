23a8b18604ffb7db35f7b8c3f76645f751cc09f6
SravsHub <107316109+SravsHub@users.noreply.github.com>
2023-02-07 14:16:51 -0500

    Update streamlit_app.py
    fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,25 @@ import streamlit
 streamlit.title("My parents new healthy dinner")
 
 
-#streamlit.header('Breakfast Menu')
-#streamlit.text('🥣 Omega3 & blueberry oatmeal')
-#streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-#streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-#streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega3 & blueberry oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
-#streamlit.header('🍌🥭 Build your own smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build your own smoothie 🥝🍇')
 
-#import pandas
-#my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list=my_fruit_list.set_index('Fruit')
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-#fruits_to_show=my_fruit_list.loc[fruits_selected]
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response =request.get("httpd://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
