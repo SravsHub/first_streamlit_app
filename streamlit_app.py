2a3d0801136c86577f244dfde302629e66fc457d
SravsHub <107316109+SravsHub@users.noreply.github.com>
2023-02-09 09:00:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,23 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-   fruit_choice=streamlit.text_input('What fruit would you like information about?','kiwi')
-#streamlit.write('The user entered',fruit_choice)
-   if not fruit_choice:
-       streamlit.error("Please select a fruit to get information.")
-   else:
+#create the repeatable code back (called a function)
+def get_fruityvice_data(this_fruit_choice):
        fruityvice_response =requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
        #normalizing the semis structured data to flat table
        fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
        #display the normalized table
-       streamlit.dataframe(fruityvice_normalized)
+       return fruityvice_normalized
+   
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+   fruit_choice=streamlit.text_input('What fruit would you like information about?')
+#streamlit.write('The user entered',fruit_choice)
+   if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+   else:
+      back_from_function=get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
    streamlit.error()
 streamlit.stop()
